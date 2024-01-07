4f807913f9e082e1d1e55c4de2a1b534ee34b2f4
cassidysimmons <cassidy1231@yahoo.com>
2024-01-07 15:12:18 -0600

    fixed query two; data analysis all done :)

--- employeeSQL/sql_challenge_queries.sql
+++ employeeSQL/sql_challenge_queries.sql
@@ -6,7 +6,7 @@ ON e.emp_no = s.emp_no;
 -- 2. List the first name, last name, and hire date for the employees who were hired in 1986.
 SELECT first_name, last_name, hire_date
 from employees
-WHERE -- hire_date = 1986;
+WHERE hire_date LIKE '%1986';
 
 -- 3. List the manager of each department along with their department number, department name, employee number, last name, and first name.
 SELECT dm.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name
